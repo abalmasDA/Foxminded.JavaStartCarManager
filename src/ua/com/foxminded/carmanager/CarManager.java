c251f6b916925da722342a90f76140ed65cffd57
Dmytro Abalmas <124811948+abalmasDA@users.noreply.github.com>
2023-02-23 21:22:26 +0200

    Update CarManager.java

--- src/ua/com/foxminded/carmanager/CarManager.java
+++ src/ua/com/foxminded/carmanager/CarManager.java
@@ -4,7 +4,7 @@ public class CarManager {
     public static void main(String[] args) {
         Car prius = new Car("Toyota Prius", 2008, 15000, 1200, Color.GREEN);
 
-        Car renault = new Car("Renault Laguna", 2000, 9700, 1600, Color.WHITE);
+        Car renault = new Car("Renault Laguna", 2000, 9700, 1600, Color.WHITE); //delete this row
         Car renault2 = new Car("Renault Laguna", 2000, 9700, 1600, Color.WHITE);
 
 
