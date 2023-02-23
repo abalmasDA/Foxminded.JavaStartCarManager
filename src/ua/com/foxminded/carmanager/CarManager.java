c239360467869bb0ad73dca341e71cee369a5e72
Dmytro Abalmas <124811948+abalmasDA@users.noreply.github.com>
2023-02-23 21:24:36 +0200

    Update CarManager.java

--- src/ua/com/foxminded/carmanager/CarManager.java
+++ src/ua/com/foxminded/carmanager/CarManager.java
@@ -4,7 +4,7 @@ public class CarManager {
     public static void main(String[] args) {
         Car prius = new Car("Toyota Prius", 2008, 15000, 1200, Color.GREEN);
 
-        Car renault = new Car("Renault Laguna", 2000, 9700, 1600, Color.WHITE); //delete this row
+        Car renault = new Car("Renault Laguna", 2000, 9700, 1600, Color.WHITE);
         Car renault2 = new Car("Renault Laguna", 2000, 9700, 1600, Color.WHITE);
 
 
@@ -12,11 +12,11 @@ public class CarManager {
         Motorcycle yamaha = new Motorcycle("Yamaha FZ1", 2007, 9000, 700, "orangE", "gas", false);
 
 
-        prius.addDistance(1000);//delete this row
+        prius.addDistance(1000);
 
 
         System.out.println(prius);
-        System.out.println(renault);//delete this row
+        System.out.println(renault);
         System.out.println(suzuki);
         System.out.println(yamaha);
 
